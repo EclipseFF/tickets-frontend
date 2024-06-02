08ca9670cf78dc84d5574086a561bf126b2a9117
EclipseFF <eclipseff@mail.ru>
2024-06-02 22:45:42 +0500

    fix

--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -1,3 +1,4 @@
+/*
 'use client';
 import React, { useEffect, useState } from 'react';
 import SeatSelection from "@/components/test/seat-selection";
@@ -41,3 +42,4 @@ export default function Page(){
     );
 };
 
+*/
