754ba92daddd82e880280f78f8ab85aea43e4786
EclipseFF <eclipseff@mail.ru>
2024-06-05 09:50:12 +0500

    fix

--- components/profile/menu-options/personal-data.tsx
+++ components/profile/menu-options/personal-data.tsx
@@ -164,7 +164,7 @@ export default function PersonalData(props: Props) {
                         name="birthday"
                         render={({ field }) => (
                             <FormItem>
-                                <FormLabel>Дата рождения: {props.additionalData?.birthdate && formatDateToRussian(props.additionalData?.birthdate.toString())}</FormLabel>
+                                <FormLabel>Дата рождения: {props.additionalData?.birthdate && props.additionalData?.birthdate.toString() && formatDateToRussian(props.additionalData?.birthdate.toString())}</FormLabel>
                                 <FormControl>
                                     <Input type={'date'} {...field}
                                            value={
